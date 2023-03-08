68b37f61513cc0d670d130f02079c9b92cdbf967
Miguel Camilo <miguelcamilo48@gmail.com>
2023-03-07 22:36:18 -0500

    updated gitignore

--- functions/src/connection/db_connection.js
+++ functions/src/connection/db_connection.js
@@ -2,6 +2,6 @@ import { MongoClient } from "mongodb";
 import { MONGO_CREDENTIALS } from "../../utils.js";
 
 export const db_connection = () => {
-    const client = new MongoClient(MONGO_CREDENTIALS.uri)
-    return client.db(MONGO_CREDENTIALS.db)
-}
+	const client = new MongoClient(MONGO_CREDENTIALS.uri);
+	return client.db(MONGO_CREDENTIALS.db);
+};
